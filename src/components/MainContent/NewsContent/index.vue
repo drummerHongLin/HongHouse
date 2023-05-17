e868b0b848f484ea591f0acf644044f4572bc6d7
drummerHongLin <1355879370@qq.com>
2023-05-17 17:36:38 +0800

    增加滚动加载功能

--- src/components/MainContent/NewsContent/index.vue
+++ src/components/MainContent/NewsContent/index.vue
@@ -1,16 +1,44 @@
 <script>
+import FirstNews from './FirstNews/index.vue'
+import HotNews from './HotNews/index.vue'
 export default {
-  name:"NewsContent"
+  name:"NewsContent",
+  data(){
+      return {
+        activeName:'hot'
+      }
+  },
+  methods:{
+    handleClick(tab,event){
+      console.log(tab, event)
+    }
+  },
+  components:{FirstNews,HotNews}
 }
 </script>
 <template>
   <div class="NewsContent">
-
+    <el-tabs v-model="activeName" class="Titles" @tab-click="handleClick">
+      <el-tab-pane class="NewsMain" label = 'HotNews' name="hot">热点欣慰</el-tab-pane>
+      <el-tab-pane class="NewsMain" label = 'FirstNews' name="first"><FirstNews/></el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 <style lang="less">
 .NewsContent {
-  height: 900px;
-  background-color: skyblue;
+  ::-webkit-scrollbar { width: 0 !important }
+  overflow: none;
+  .infinite-list-item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+  background: var(--el-color-primary-light-9);
+  margin: 10px;
+  color: var(--el-color-primary);
+}
+ .infinite-list-item + .list-item {
+  margin-top: 10px;
+}
 }
 </style>
